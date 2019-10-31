11add69019f241aa464780ef6a9306ec8b62cb14
Eric <ericmuchiri02@gmail.com>
2019-10-31 07:53:37 +0300

    feature: adding routing

--- 02express-setup/express-example/app.js
+++ 02express-setup/express-example/app.js
@@ -16,6 +16,16 @@ app.use(function(req, res, next){
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//routes
+//route for a get request
+app.get('/', function(req, res){
+    res.send('Hello world!');
+});
+//route for an about page
+app.get('/about', function(req, res){
+    res.send('About page');
+});
+
 app.listen(port);
 console.log("Server started on port "+port);
 
