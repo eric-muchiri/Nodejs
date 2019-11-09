84bf7ceeff642e37aed34d4bb7acc8d5a51cff5c
Eric <ericmuchiri02@gmail.com>
2019-11-09 15:29:13 +0300

    progress: using pug for render

--- 03Template-engines/express-example/app.js
+++ 03Template-engines/express-example/app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var pug = require('pug');
 
 var port = 3000;
 
@@ -12,6 +13,10 @@ app.use(function(req, res, next){
     console.log("Time: ", Date.now());
     next();
 });
+
+//set the view engine(after installing pug)
+app.set('view engine', 'pug');
+
 //body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,7 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 //routes
 //route for a get request
 app.get('/', function(req, res){
-    res.send('Hello world!');
+    // res.send('Hello world!');
+    res.render('index', {
+        //dynamic values
+        title: 'Hello World',
+        showTitle: false
+    });
 });
 //route for an about page
 app.get('/about', function(req, res){
