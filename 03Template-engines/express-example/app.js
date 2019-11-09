15b6e4c19e54e7330ae0a094b300366f9f4b226a
Eric <ericmuchiri02@gmail.com>
2019-11-09 09:20:53 +0300

    starter from previous progress

---
+++ 03Template-engines/express-example/app.js
@@ -0,0 +1,33 @@
+//required packages
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var port = 3000;
+
+var app = express();
+//example middleware
+//express
+app.use(function(req, res, next){
+    console.log("Time: ", Date.now());
+    next();
+});
+//body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+//route for a get request
+app.get('/', function(req, res){
+    res.send('Hello world!');
+});
+//route for an about page
+app.get('/about', function(req, res){
+    res.send('About page');
+});
+
+app.listen(port);
+console.log("Server started on port "+port);
+
+module.exports = app;
