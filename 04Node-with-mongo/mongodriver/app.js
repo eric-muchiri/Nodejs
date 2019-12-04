4899ad4d7883a9e1387095fa956aa70573ba9582
Eric <ericmuchiri02@gmail.com>
2019-12-04 10:57:54 +0300

    mongo driver

--- 04Node-with-mongo/mongodriver/app.js
+++ 04Node-with-mongo/mongodriver/app.js
@@ -6,4 +6,25 @@ MongoClient.connect(url, function(err, db){
         return console.dir(err);
     }
     console.log("connected to Mongodb");
+
+    InsertDocument(db, function(){
+        db.close();
+    });
 });
+//insert single doc
+const InsertDocument = function(db, callback){
+    //get collection
+    const collection = db.collection('users');
+    //insert Docs
+    collection.insert({
+        name: 'Eric Muchiri',
+        email: 'test@gmail.com'
+    }, function(err, result){
+        if(err){
+            return console.dir(err);
+        }
+        console.log('Inserted Document');
+        console.log(result);
+        callback(result);
+    });
+}
